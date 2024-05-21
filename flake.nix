6b21078f1d68dcf97bceb05a41f0ee854cb97f3b
Jost Schulte <jost.schulte@protonmail.com>
2024-05-21 13:33:15 -0600

    chore: set flake inputs to holochain v0.2 and lair v0.4.2

--- flake.nix
+++ flake.nix
@@ -23,13 +23,13 @@
 
     # Holochain sources
     holochain-src = {
-      url = "github:holochain/holochain";
+      url = "github:holochain/holochain/main-0.2";
       flake = false;
     };
 
     # Lair keystore sources
     lair-keystore-src = {
-      url = "github:holochain/lair";
+      url = "github:holochain/lair/lair_keystore-v0.4.2";
       flake = false;
     };
   };
