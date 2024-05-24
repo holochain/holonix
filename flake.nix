6c46f6f60c3943a0f82d212e7cd99f31c2bbe063
Jost Schulte <jost.schulte@protonmail.com>
2024-05-24 10:26:01 -0600

    nix: use patched nixpkgs

--- flake.nix
+++ flake.nix
@@ -18,7 +18,6 @@
 
     # Rust toolchain
     rust-overlay = {
-      # url = "github:jost-s/rust-overlay/fix/return-target-dependency-as-flat-list-for-darwin-std";
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -161,19 +160,8 @@
                   # additional packages needed for darwin platforms
                   pkgs.darwin.apple_sdk.frameworks.Carbon
                   pkgs.darwin.apple_sdk.frameworks.WebKit
-                  # pkgs.darwin.apple_sdk_11_0.frameworks.Carbon
-                  # pkgs.darwin.apple_sdk_11_0.frameworks.WebKit
-                  # additional packages needed for darwin platforms on x86_64
-                  # pkgs.darwin.apple_sdk_10_12.frameworks.AppKit
-                  # pkgs.darwin.apple_sdk_10_12.frameworks.CoreFoundation
-                  # pkgs.darwin.apple_sdk_10_12.frameworks.CoreServices
-                  # pkgs.darwin.apple_sdk_10_12.frameworks.Security
-                  # pkgs.darwin.apple_sdk_10_12.frameworks.IOKit
-                  # pkgs.darwin.apple_sdk_10_12.frameworks.WebKit
-                  # pkgs.glib
-                  # pkgs.pkg-config
-                  # pkgs.xcbuild
-                  # pkgs.libiconv
+                  pkgs.darwin.apple_sdk.libs.xpc
+                  pkgs.libiconv
                 ]);
               # do not check built package as it either builds successfully or not
               doCheck = false;
