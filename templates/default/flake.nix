cf151a5c7b8fe1597d744b6930536cfd28c9a53b
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-06-05 18:15:42 +0100

    Fix template

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -8,26 +8,26 @@
     flake-parts.follows = "holonix/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = builtins.attrNames inputs.holonix.devShells;
-      perSystem =
-        { inputs'
-        , pkgs
-        , ...
-        }:
-        {
-          formatter = pkgs.nixpkgs-fmt;
+  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = builtins.attrNames inputs.holonix.devShells;
+    perSystem = { inputs', pkgs, ... }: {
+      formatter = pkgs.nixpkgs-fmt;
 
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ inputs'.holonix.devShells ];
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [ inputs'.holonix.devShells ];
 
-            packages = with pkgs; [
-              nodejs_20 # For UI development
-              binaryen # For WASM optimisation
-              # Add any other packages you need here
-            ];
-          };
-        };
+        packages = (with inputs'.holonix.packages; [
+          holochain
+          lair-keystore
+          hc-launch
+          hc-scaffold
+          rust # For Rust development, with the WASM target included for zome builds
+        ]) ++ (with pkgs; [
+          nodejs_20 # For UI development
+          binaryen # For WASM optimisation
+          # Add any other packages you need here
+        ]);
+      };
     };
+  };
 }
