134e57a1bb1e645f9d73b4ddb01e6e43a6b32460
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-07-01 19:46:50 +0100

    Update versions (#16)
    * Update versions
    
    * More space for build

--- flake.nix
+++ flake.nix
@@ -23,13 +23,13 @@
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain/main";
+      url = "github:holochain/holochain/holochain-0.4.0-dev.10";
       flake = false;
     };
 
     # Lair keystore sources
     lair-keystore = {
-      url = "github:holochain/lair/lair_keystore-v0.4.4";
+      url = "github:holochain/lair/lair_keystore-v0.4.5";
       flake = false;
     };
 
