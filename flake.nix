be495a65908e7d275b273ec81e64d81ecaff5e70
Holochain Release Automation <100725712+holochain-release-automation2@users.noreply.github.com>
2024-07-10 17:26:53 +0200

    [create-pull-request] automated change (#19)
    Co-authored-by: ThetaSinner <6267702+ThetaSinner@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain/holochain-0.4.0-dev.11";
+      url = "github:holochain/holochain/holochain-0.4.0-dev.12";
       flake = false;
     };
 
