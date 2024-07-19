792f437f420a15e4f674d5d545dcc001c37f2bea
ThetaSinner <greatdjonfire@hotmail.co.uk>
2024-07-19 22:33:58 +0100

    Update versions for 0.3

--- templates/holo/flake.nix
+++ templates/holo/flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for Holo-enabled Holochain app development";
 
   inputs = {
-    holonix.url = "github:holochain/holonix?ref=main";
+    holonix.url = "github:holochain/holonix?ref=main-0.3";
 
     nixpkgs.follows = "holonix/nixpkgs";
     flake-parts.follows = "holonix/flake-parts";
