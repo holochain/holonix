d98738d46bc534f3a01de5f4cf374c417b39bb17
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-09-24 13:59:21 +0300

    Improve hc-scaffold cranelib filters (#49)

--- flake.nix
+++ flake.nix
@@ -241,7 +241,7 @@
             hc-scaffold =
               let
                 # Crane filters out all non-cargo related files. Define include filter with files needed for build.
-                nonCargoBuildFiles = path: _type: builtins.match ".*(gitignore|md)$" path != null;
+                nonCargoBuildFiles = path: _type: builtins.match ".*(gitignore|md|hbs)$" path != null;
                 includeFilesFilter = path: type:
                   (craneLib.filterCargoSources path type) || (nonCargoBuildFiles path type);
               in
