6db1468c1d2cc47419c1bf2ee715c208b97dedb0
holochain-release-automation2 <100725712+holochain-release-automation2@users.noreply.github.com>
2025-02-12 02:41:06 +0000

    [create-pull-request] automated change

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain/holochain-0.5.0-dev.17";
+      url = "github:holochain/holochain/holochain-0.5.0-dev.18";
       flake = false;
     };
 
