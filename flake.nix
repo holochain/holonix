68944471282206753c880954ffa269d9601ed330
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-04-15 12:00:49 +0100

    chore: Update to latest RC 0.5.0-rc.3

--- flake.nix
+++ flake.nix
@@ -21,13 +21,13 @@
     };
 
     kitsune2 = {
-      url = "github:holochain/kitsune2?ref=v0.1.1";
+      url = "github:holochain/kitsune2?ref=v0.1.2";
       flake = false;
     };
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain?ref=holochain-0.5.0-rc.1";
+      url = "github:holochain/holochain?ref=holochain-0.5.0-rc.3";
       flake = false;
     };
 
