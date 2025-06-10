5d80124990af45c38ac102e90f805e81d7f1be99
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-06-10 12:08:22 +0100

    chore: Try building on old macos without support

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
     flake-parts.lib.mkFlake { inherit inputs; }
       {
         # systems that his flake can be used on
-        systems = [ "aarch64-darwin" "x86_64-linux" "aarch64-linux" ];
+        systems = [ "aarch64-darwin" "x86_64-linux" "x86_64-darwin" "aarch64-linux" ];
 
         # for each system...
         perSystem = { config, pkgs, system, ... }:
