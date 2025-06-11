4aa980b69e8edd16a9e1fd407b148cabb3e5a45d
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-06-11 11:09:58 +0100

    chore: Match Rust version from Holochain

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
               inherit system overlays;
             };
 
-            rustVersion = "1.83.0";
+            rustVersion = "1.87.0";
 
             # define Rust toolchain version and targets to be exported from this flake
             rust = (pkgs.rust-bin.stable.${rustVersion}.minimal.override
