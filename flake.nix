4548d65e5f11a0a0cbe80bf2ffcf2e9ec084e51c
ThetaSinner <6267702+ThetaSinner@users.noreply.github.com>
2025-07-09 19:44:08 +0000

    chore: Update Holonix versions

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain?ref=holochain-0.5.3";
+      url = "github:holochain/holochain?ref=holochain-0.5.4";
       flake = false;
     };
 
