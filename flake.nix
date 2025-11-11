89b9222e582e179f29ecb83a705be810b3ad6ec9
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-11-11 15:32:29 +0000

    chore: Bump to K2 0.3.1 and Holochain 0.6.0-rc.1

--- flake.nix
+++ flake.nix
@@ -21,13 +21,13 @@
     };
 
     kitsune2 = {
-      url = "github:holochain/kitsune2?ref=v0.3.0";
+      url = "github:holochain/kitsune2?ref=v0.3.1";
       flake = false;
     };
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain?ref=holochain-0.6.0-rc.0";
+      url = "github:holochain/holochain?ref=holochain-0.6.0-rc.1";
       flake = false;
     };
 
