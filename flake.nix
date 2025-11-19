842f1a1d2605f7605a1f4b608aedd0f582a40420
matthme <36768177+matthme@users.noreply.github.com>
2025-11-19 17:33:06 +0000

    chore: Update Holonix versions (#229)

--- flake.nix
+++ flake.nix
@@ -21,13 +21,13 @@
     };
 
     kitsune2 = {
-      url = "github:holochain/kitsune2?ref=v0.3.1";
+      url = "github:holochain/kitsune2?ref=v0.3.2";
       flake = false;
     };
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain?ref=holochain-0.6.0-rc.1";
+      url = "github:holochain/holochain?ref=holochain-0.6.0";
       flake = false;
     };
 
