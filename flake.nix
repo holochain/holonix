f8a3b9496c517674c15a4a0e76e645f11fb80466
Paul d'Aoust <paul.daoust@holo.host>
2025-12-01 09:59:52 -0800

    chore: update scaffolding to 0.600.1

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
     # Holochain scaffolding CLI
     hc-scaffold = {
-      url = "github:holochain/scaffolding?ref=v0.600.0";
+      url = "github:holochain/scaffolding?ref=v0.600.1";
       flake = false;
     };
 
