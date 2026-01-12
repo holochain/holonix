09c1d320ef5afe1d5c1c543c67b5da7d64bc753b
Jost Schulte <jost-s@users.noreply.github.com>
2026-01-12 16:55:54 -0600

    chore: update to holochain v0.7.0-dev.7

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain?ref=holochain-0.7.0-dev.4";
+      url = "github:holochain/holochain?ref=holochain-0.7.0-dev.7";
       flake = false;
     };
 
