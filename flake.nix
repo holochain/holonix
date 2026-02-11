b1d4c68dded9eafa475aa97ee138bbeaefd97e84
Matt <matt@buildyourweb.app>
2026-02-11 15:02:26 -0700

    chore: rm playground

--- flake.nix
+++ flake.nix
@@ -42,12 +42,6 @@
       url = "github:holochain/scaffolding?ref=v0.600.1";
       flake = false;
     };
-
-    # Third-party tool from Darksoil Studio for exploring DHT data.
-    playground = {
-      url = "github:darksoil-studio/holochain-playground?ref=main";
-      flake = false;
-    };
   };
 
   # outputs that this flake should produce
@@ -237,62 +231,6 @@
                 ];
               };
 
-            # Define how to build the Holochain Playground CLI.
-            hc-playground =
-              let
-                # Filter down to required files for the CLI build.
-                pnpmFilter = path: type: type == "directory" || builtins.match ".*(ts|js|json|yaml|html)$" path != null;
-
-                cli = pkgs.stdenv.mkDerivation (finalAttrs: {
-                  pname = "hc-playground";
-                  # See the `package.json` version for the source code being imported.
-                  version = "0.500.0";
-
-                  src = pkgs.lib.sources.cleanSourceWith {
-                    src = inputs.playground;
-                    filter = pnpmFilter;
-                  };
-
-                  nativeBuildInputs = with pkgs; [
-                    nodejs
-                    pnpm_9.configHook
-                  ];
-
-                  # See what is required by the `pnpm build:cli` command.
-                  # This filters down to just the dependencies needed for the CLI.
-                  pnpmWorkspaces = [
-                    "@holochain-playground/simulator"
-                    "@holochain-playground/elements"
-                    "@holochain-playground/cli-client"
-                    "@holochain-playground/cli"
-                  ];
-
-                  # Fetch dependencies with a fixed-output-derivation.
-                  #
-                  # When updating the input, this fixed derivation will need to be updated.
-                  # If you leave this hash alone, you'll get errors about dependencies needing to be
-                  # fetched in the build phase.
-                  pnpmDeps = pkgs.pnpm_9.fetchDeps {
-                    inherit (finalAttrs) pname version src;
-                    hash = "sha256-gakSG1K/DkS/7pt5PCdS9ODsUEiv56ZkHBdFcJgmlk4=";
-                    fetcherVersion = 1;
-                  };
-
-                  buildPhase = ''
-                    runHook preBuild
-                    pnpm build:cli
-                    runHook postBuild
-
-                    mkdir $out
-                    cp -R packages/cli/server/dist $out
-                  '';
-                });
-              in
-              # Turn the built JS into a runnable CLI.
-              pkgs.writeShellScriptBin "hc-playground" ''
-                node ${cli}/dist/app.js "$@"
-              '';
-
             # Shell script to show what versions of the Holochain tools are installed.
             # It can be included as a package into the dev shell and is then available by its name - `hn-introspect`.
             hn-introspect = pkgs.writeShellScriptBin "hn-introspect" ''
@@ -351,7 +289,6 @@
               inherit hc-scaffold;
               inherit rust;
               inherit hn-introspect;
-              inherit hc-playground;
             };
 
             # Define runnable applications for use with `nix run`.
@@ -370,8 +307,6 @@
               lair-keystore.meta.description = "Lair keystore";
               hc-scaffold.program = "${hc-scaffold}/bin/hc-scaffold";
               hc-scaffold.meta.description = "Holochain scaffolding CLI";
-              hc-playground.program = "${hc-playground}/bin/hc-playground";
-              hc-playground.meta.description = "Holochain Playground";
             };
 
             devShells = {
@@ -384,7 +319,6 @@
                   lair-keystore
                   hc-scaffold
                   hn-introspect
-                  hc-playground
                   rust
                 ];
               };
