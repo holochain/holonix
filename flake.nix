3968c69589bdd2ca96145ed807f2129313d21093
Jost <jost-s@users.noreply.github.com>
2026-04-03 19:09:14 -0600

    feat: upgrade to holochain v0.6.1-rc.6

--- flake.nix
+++ flake.nix
@@ -21,13 +21,13 @@
     };
 
     kitsune2 = {
-      url = "github:holochain/kitsune2?ref=v0.4.0-dev.7";
+      url = "github:holochain/kitsune2?ref=v0.4.0-dev.9";
       flake = false;
     };
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain?ref=holochain-0.6.1-rc.5";
+      url = "github:holochain/holochain?ref=holochain-0.6.1-rc.6";
       flake = false;
     };
 
