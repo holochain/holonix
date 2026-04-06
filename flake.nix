6a4c497c601a348ed0eb7fcf081c8b4194e4bd45
holochain-release-automation2 <100725712+holochain-release-automation2@users.noreply.github.com>
2026-04-06 01:53:32 +0000

    chore: Update Holonix versions

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
     # Holochain sources
     holochain = {
-      url = "github:holochain/holochain?ref=holochain-0.7.0-dev.18";
+      url = "github:holochain/holochain?ref=holochain-0.7.0-dev.19";
       flake = false;
     };
 
