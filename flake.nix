43e1298c7be71e7a1e98c94a9935ff03c1b40427
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-05-14 15:09:56 +0100

    chore: Update Scaffolding to 0.601.0

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
     # Holochain scaffolding CLI
     hc-scaffold = {
-      url = "github:holochain/scaffolding?ref=v0.600.3-rc.0";
+      url = "github:holochain/scaffolding?ref=v0.601.0";
       flake = false;
     };
   };
